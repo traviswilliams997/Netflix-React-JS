f1ec614abba2348ec381b6b5997be34d7886f156
Travis Williams <traviswilliams997@gmail.com>
2022-09-27 21:31:27 -0500

    Store data from api request

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import requests  from '../Request'
 
 const Main = () => {
     const [movies, setMovies] = useState([])
+
+    const movie = movies[Math.floor(Math.random()* movies.length)]
+
+    useEffect(() =>  {
+
+        axios.get(requests.requestPopular).then((response) => {
+            setMovies(response.data.results)
+        })
+    }, [])
+    console.log(movie)
     
+
 
   return (
     <div>Main</div>
