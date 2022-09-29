7945972f2e69b97d2842155fe615fb3313fde212
Travis Williams <traviswilliams997@gmail.com>
2022-09-29 12:32:45 -0500

    Add way to protect account page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
 import { AuthContextProvider } from "./context/AuthContext";
 import Account from "./pages/Account";
 import Home from "./pages/Home";
@@ -16,7 +17,13 @@ function App() {
         <Route path ='/' element = {<Home />} />
         <Route path='/login' element = {<Login />} />
         <Route path='/signup' element = {<Signup />} />
-        <Route path ='/account' element = {<Account /> } /> 
+        <Route path ='/account' 
+        element = {
+        <ProtectedRoute>
+          <Account /> 
+          </ProtectedRoute>
+        } 
+        /> 
       </Routes>
       </AuthContextProvider>
     </>
