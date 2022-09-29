4989196c1ee69dbd5f2b4223e516c2e4b0d4fa58
Travis Williams <traviswilliams997@gmail.com>
2022-09-28 21:22:28 -0500

    Add firebase

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { AuthContextProvider } from "./context/AuthContext";
 import Home from "./pages/Home";
 
 
 function App() {
   return (
     <>
+    <AuthContextProvider>
       <Navbar />
       <Routes>
         <Route path ='/' element = {<Home />} />
       </Routes>
-     
+      </AuthContextProvider>
     </>
   )
 }
